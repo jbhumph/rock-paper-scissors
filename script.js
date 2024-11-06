57cd1e8236410ff8b142d8b26bc13ab10a4925f4
John <jbhumph@gmail.com>
2024-11-06 14:59:01 -0800

    Copied over program from initial SD250 files.

---
+++ script.js
@@ -0,0 +1,209 @@
+// declare game variables
+let player = "none"
+let computer = "none"
+let status = "none"
+// declare document items
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const new_game = document.querySelector("#new_game");
+const play = document.querySelector("#play_button");
+const pA = document.querySelector("#pA");
+const pB = document.querySelector("#pB");
+const pAScore = document.querySelector("#pAScore");
+const pBScore = document.querySelector("#pBScore");
+const currentStatus = document.querySelector("#currentStatus");
+
+let selection = "none";
+let comp_sel = "tbd";
+let ng = false;
+let ready = false;
+
+let score_p = 0;
+let score_c = 0;
+
+rock.addEventListener("click", () => {
+    if (ng === false) {
+        if (selection !== "rock") {
+            selection = "rock"
+            rock.classList.add("selected")
+            paper.classList.remove("selected");
+            scissors.classList.remove("selected");
+            ready = true;
+            update();
+        } else {
+            selection = "none";
+            rock.classList.remove("selected");
+            ready = false
+            update();
+            countdown();
+        }
+    }
+
+})
+paper.addEventListener("click", () => {
+    if (ng === false) {
+        if (selection !== "paper") {
+            selection = "paper"
+            paper.classList.add("selected")
+            rock.classList.remove("selected");
+            scissors.classList.remove("selected");
+            ready = true;
+            update();
+        } else {
+            selection = "none";
+            paper.classList.remove("selected");
+            ready = false
+            update();
+            countdown();
+        }
+    }
+})
+scissors.addEventListener("click", () => {
+    if (ng === false) {
+        if (selection !== "scissors") {
+            selection = "scissors"
+            scissors.classList.add("selected")
+            rock.classList.remove("selected");
+            paper.classList.remove("selected");
+            ready = true;
+            update();
+        } else {
+            selection = "none";
+            scissors.classList.remove("selected");
+            ready = false
+            update();
+            countdown();
+        }
+    }
+})
+
+play.addEventListener("click", () => {
+    if (ready === true) {
+        playGame(selection);
+    }
+})
+
+new_game.addEventListener("click", () => {
+    if (ng === true) {
+        ng = false;
+        selection = "none";
+        rock.classList.remove("selected")
+        paper.classList.remove("selected");
+        scissors.classList.remove("selected");
+        currentStatus.id = "currentStatus";
+        currentStatus.innerHTML = "15"
+        comp_sel = "tbd";
+        update();
+        countdown();
+    }
+})
+
+function getRandom() {
+    // create a random number that then returns RPS
+    let number = Math.floor(Math.random() * 3)
+    if (number === 0) {
+        return "rock";
+    } else if (number === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+function playGame(choice) {
+    // primary game logic
+    player = choice;
+    computer = getRandom()
+
+    if (player === computer) {
+        status = "It's a draw"
+    } else if (computer === "rock") {
+        if (player === "paper") {
+            status = "Player Wins"
+            score_p++;
+        } else {
+            status = "Computer Wins"
+            score_c++;
+        }
+    } else if (computer === "paper") {
+        if (player === "scissors") {
+            status = "Player Wins"
+            score_p++;
+        } else {
+            status = "Computer Wins"
+            score_c++;
+        }
+    } else if (computer === "scissors") {
+        if (player === "rock") {
+            status = "Player Wins"
+            score_p++;
+        } else {
+            status = "Computer Wins"
+            score_c++;
+        }
+    }
+    comp_sel = computer;
+    ng = true
+    update()
+    play.classList.remove("lit");
+    ready = false;
+    currentStatus.id = "win_lit";
+    currentStatus.innerHTML = status;
+}
+
+function update() {
+    if (ready === true) {
+        play.classList.add("lit")
+        pA.innerHTML = `Player:   ${selection}`;
+        pB.innerHTML = `Computer:   ${comp_sel}`;
+    } else {
+        play.classList.remove("lit")
+        pA.innerHTML = "Pick rock, paper, or scissors and press play!";
+        pB.innerHTML = "";
+    }
+    if (ng === true) {
+        new_game.classList.add("lit");
+    } else {
+        new_game.classList.remove("lit");
+    }
+    pAScore.innerText = `Player: ${score_p}`
+    pBScore.innerText = `Computer: ${score_c}`
+}
+
+function countdown() {
+    let number = 15;
+    const counter = setInterval(() => {
+        number --;
+        if (number === 4) {
+            currentStatus.innerHTML = "Rock";
+        } else if (number === 3) {
+            currentStatus.innerHTML = "Paper";
+        } else if (number === 2) {
+            currentStatus.innerHTML = "Scissors";
+        } else if (number === 1) {
+            currentStatus.innerHTML = "Shoot!";
+        } else {
+            currentStatus.innerHTML = number.toString();
+        }
+
+        if (selection !== "none") {
+            clearInterval(counter);
+            currentStatus.innerHTML = "Press play!"
+        }
+        if (number <= 0) {
+            clearInterval(counter);
+            selection = getRandom();
+            playGame(selection);
+            if (selection === "scissors") {
+                scissors.classList.add("selected");
+            } else if (selection === "rock") {
+                rock.classList.add("selected");
+            } else if (selection === "paper") {
+                paper.classList.add("selected");
+            }
+        }
+    },1000)
+}
+
+countdown();
